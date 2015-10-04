c7ae5d02e5945e637a9ab2bbc8fc753252f761c6
Wes Kelly <weskelly@mac.com>
2015-10-03 23:55:38 -0400

    Partial input file functionality

--- ChangeMaker/src/changemaker/ChangeMaker.java
+++ ChangeMaker/src/changemaker/ChangeMaker.java
@@ -5,18 +5,79 @@
  */
 package changemaker;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author wes
  */
-public class ChangeMaker 
+public class ChangeMaker
 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private int[] denominations;
+    
+    public ChangeMaker(int denominations_[])
+        throws InvalidDenominationException
+    {
+        if (denominations_ == null)
+        {
+            String err = "";
+            err += "Denomination array passed was null. Check your input file";
+            throw new InvalidDenominationException(err);
+        }
+        
+        denominations = denominations_;
     }
     
+    private static int[] getDenominationsFromFile(Scanner inputFile)
+        throws InvalidDenominationException
+    {
+        int numOfDenominations = 0;
+        if (inputFile.hasNextInt())
+        {
+            numOfDenominations = inputFile.nextInt();
+        }
+        else
+        {
+            String err = "No denomination info found in file, check file.";
+            throw new InvalidDenominationException(err);
+        }
+        
+        int[] denominations = new int[numOfDenominations];  
+        
+        for (int i = 0; i < numOfDenominations; i++)
+        {
+            if (inputFile.hasNextInt())
+            {
+                denominations[i] = inputFile.nextInt();
+            }
+            else
+            {
+                String err = "No denomination info found in file, check file.";
+                throw new InvalidDenominationException(err);
+            }
+        }
+            
+        //TODO: get more numbers
+        
+        return null;
+    }
+    
+    public static void main(String[] args)
+    {
+        try
+        {
+            Scanner fileScanner = new Scanner(new File("make_change_input.txt"));
+            
+            int[] denominations = getDenominationsFromFile(fileScanner);
+
+            ChangeMaker chg = new ChangeMaker(denominations);
+        }
+        catch (InvalidDenominationException | FileNotFoundException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        
+    }
 }
